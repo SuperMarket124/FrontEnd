bdc6d57b45b1064ef1838aa8bc9e95e6079e1074
yoon1 <o_ink@naver.com>
2021-06-19 00:38:43 +0900

    Try api request in loginform

--- src/components/form/LoginForm.js
+++ src/components/form/LoginForm.js
@@ -1,29 +1,89 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import FormButton from "../../components/FormButton";
 import FormInputText from "../../components/FormInputText";
+import axios from "axios";
 
-// TODO a -> LINK
 const LoginForm = () => {
+  const [token, setToken] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [inputs, setInputs] = useState({
+    id: "",
+    password: "",
+  });
+
+  const { id, password } = inputs;
+
+  const login = async () => {
+    try {
+      // 초기화
+      setError(null);
+      setToken(null);
+      setLoading(true);
+      const response = await axios.post(
+        "http://ydhdelivery.shop/authenticate",
+        {
+          name: id,
+          password: password,
+        }
+      );
+      // if (response.status === "200") {
+      //   setToken(response.data.token);
+      //   sessionStorage.setItem("token", response.data.token);
+      // } else {
+      //   alert("password erorr");
+      // }
+      setToken(response.data.token);
+      sessionStorage.setItem("token", response.data.token);
+    } catch (e) {
+      setError(e);
+    }
+    setLoading(false);
+  };
+
+  const onInputChange = (e) => {
+    const { value, name } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value,
+    });
+  };
+
+  if (loading) return <div>로딩중..</div>;
+  if (error) return <div>에러가 발생했습니다</div>;
+
   return (
     <LoginSection>
       <h3>로그인</h3>
       <WriteForm>
         <LoginView>
           <form>
-            <FormInputText type="text" placeholder="아이디를 입력해주세요" />
+            <FormInputText
+              type="text"
+              placeholder="아이디를 입력해주세요"
+              name="id"
+              value={id}
+              onChange={onInputChange}
+            />
             <FormInputText
               type="password"
+              name="password"
               placeholder="비밀번호를 입력해주세요"
+              value={password}
+              onChange={onInputChange}
             />
             <LoginSearch>
               <a>아이디 찾기</a>
               <Bar></Bar>
               <a>비밀번호 찾기</a>
             </LoginSearch>
-            <FormButton color="purple">로그인</FormButton>
+            <FormButton color="purple" onClick={login} type="">
+              로그인
+            </FormButton>
+            <div>{token}</div>
           </form>
-          <FormButton color="white">회원가입</FormButton>
+          <RegisterButton color="white">회원가입</RegisterButton>
         </LoginView>
       </WriteForm>
     </LoginSection>
@@ -40,6 +100,13 @@ const LoginSection = styled.section`
   font-family: note sans;
   color: #333;
   text-align: center;
+
+  h3 {
+    font-weight: 800;
+    font-size: 20px;
+    line-height: 20px;
+    text-align: center;
+  }
 `;
 
 const WriteForm = styled.div`
@@ -58,7 +125,6 @@ const LoginSearch = styled.div`
   overflow: hidden;
   width: 340px;
   padding-top: 13px;
-  margin-top: 9px;
   padding-bottom: 28px;
   display: flex;
   justify-content: flex-end;
@@ -74,4 +140,8 @@ const Bar = styled.div`
   height: 10px;
   margin: 3px 6px 0;
   background-color: #333;
+`;
+
+const RegisterButton = styled(FormButton)`
+  margin-top: 10px;
 `;
